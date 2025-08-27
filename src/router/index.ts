ab9a6cf081b9e6b11c3e3cc74ee696bffba2961d
Matheus-Lara <matheusodelara@gmail.com>
2025-08-27 20:40:33 -0300

    feat: use user state in authentication and route loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 import { useAuthStore } from '@/stores/auth'
+import { useUserStore, type User } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
+  useUserStore()
 
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
     next({ name: 'login', query: to.query })
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import apiClient from '@/services/api'
 import router from '@/router' // Importando o router para o logout
+import { useUserStore } from '@/stores/user'
 
 export const useAuthStore = defineStore('auth', {
   // O estado inicial agora lê o token do localStorage
@@ -14,11 +15,14 @@ export const useAuthStore = defineStore('auth', {
     async login(credentials: { email: string; password: string }): Promise<void> {
       try {
         const response = await apiClient.post('/user/auth/login', credentials)
-        const token = response.data.token
+        const { token, user } = response.data
 
-        if (token) {
+        if (token && user) {
           this.token = token
           localStorage.setItem('authToken', token)
+
+          const userStore = useUserStore()
+          userStore.setUser(user)
         } else {
           throw new Error('Token não encontrado na resposta da API')
         }
@@ -28,9 +32,11 @@ export const useAuthStore = defineStore('auth', {
       }
     },
     logout() {
+      const userStore = useUserStore()
       this.token = null
-      localStorage.removeItem('authToken') // Removendo do localStorage
-      router.push({ name: 'login' }) // Redireciona para o login
+      localStorage.removeItem('authToken')
+      userStore.clearUser()
+      router.push({ name: 'login' })
     }
   }
 })
