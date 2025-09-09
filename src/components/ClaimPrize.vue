4c3abd08cd3db2c71d62880ead22c12f5b8a1012
Matheus-Lara <matheusodelara@gmail.com>
2025-09-09 00:47:13 -0300

    feat: implementa estrutura para resgatar as tags dos jovens

--- src/components/ClaimPrize.vue
+++ src/components/ClaimPrize.vue
@@ -1,11 +1,75 @@
 <template>
-  <div class="claim-prize-container">
-    <h2>Resgate de Prêmios</h2>
-
+  <div class="claim-prize-container container mt-4">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h2 class="text-center">Resgate de Prêmios</h2>
+        <div class="input-group mt-3">
+          <input
+            type="text"
+            v-model="userTag"
+            class="form-control"
+            placeholder="Digite a tag do jovem"
+          />
+          <button class="btn btn-primary" @click="claimPrize">Resgatar</button>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center mt-4">
+      <div class="col-md-6">
+        <h2 class="text-center">Últimas tags resgatadas</h2>
+        <div class="mt-3">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="tag in reversedLastTags" :key="tag">
+              #{{ tag }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import prizeService from '@/services/prizeService'
+import { ref, onMounted, computed } from 'vue'
+
+const userTag = ref('')
+const lastTags = ref<string[]>([])
+
+const reversedLastTags = computed(() => [...lastTags.value].reverse())
+
+const claimPrize = async () => {
+  if (!userTag.value) {
+    alert('Por favor, insira a tag do jovem.')
+    return
+  }
+  try {
+    const treatedUserTag = userTag.value.replace(/#/g, '')
+    await prizeService.claimPrizeForUser(treatedUserTag)
+    alert('Prêmio resgatado com sucesso para a tag: ' + treatedUserTag)
+
+    lastTags.value.push(treatedUserTag)
+
+    // limit to 10 items
+    if (lastTags.value.length > 10) {
+      lastTags.value.shift()
+    }
+
+    localStorage.setItem('lastProcessedTags', JSON.stringify(lastTags.value))
+
+  } catch (error: any) {
+    alert(
+      error?.error?.message || 'Ocorreu um erro ao resgatar o prêmio. Verifique com o administrador.'
+    )
+  } finally {
+    userTag.value = ''
+  }
+}
+
+onMounted(() => {
+  lastTags.value = JSON.parse(localStorage.getItem('lastProcessedTags') || '[]')
+})
+</script>
 
 <style scoped>
 .claim-prize-container {
@@ -13,6 +77,5 @@
   border-radius: 8px;
   background-color: #f8f9fa;
   border: 1px solid #dee2e6;
-  margin-top: 20px;
 }
 </style>
